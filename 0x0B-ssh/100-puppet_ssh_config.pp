6221e45762cfe9f2af9a05d7b35e8064b2573d3a
DD1C74707 <gracevalley101@gmail.com>
2023-09-23 20:41:50 -0700

    Change advance implementation

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # Set up server configuration with puppet
 file_line { '~/.ssh/config':
   path => present,
-  line => Host 18.235.248.71
-          HostName 18.235.248.71
+  line => 'Host 18.235.248.71
+      	  HostName 18.235.248.71
           User ubuntu
           IdentityFile ~/.ssh/school
-          PasswordAuthentication no,
+          PasswordAuthentication no',
 }
 
