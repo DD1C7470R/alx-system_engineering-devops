e3a883a87124c12777422156e0af3df27dea4aac
DD1C74707 <gracevalley101@gmail.com>
2023-09-23 20:47:07 -0700

    Change advance implementation

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,10 @@
 # Set up server configuration with puppet
-file_line { '~/.ssh/config':
-  path => '~/.ssh/config',
+# Set up server configuration with puppet
+file_line{'Set alias name':
+  path => '/etc/ssh/ssh_config',
   line => 'Host 18.235.248.71
-      	  HostName 18.235.248.71
-          User ubuntu
-          IdentityFile ~/.ssh/school
-          PasswordAuthentication no',
+    HostName 18.235.248.71
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no',
 }
