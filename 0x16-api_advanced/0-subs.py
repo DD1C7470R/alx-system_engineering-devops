523039957ab40f577bc8d2268d62fdce83c128d2
DD1C74707 <gracevalley101@gmail.com>
2023-11-07 09:07:45 -0800

    Fix user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x20.api.advanced:v1.0.0 (by /ubuntu)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, allow_redirects=False)
     if response.status_code == 404:
